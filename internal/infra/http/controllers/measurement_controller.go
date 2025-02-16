8342f72be55c6fca0f93d12ccc27a59735c610da
BohdanBoriak <lightraven72@gmail.com>
2025-02-16 09:40:53 +0200

    Fix: meas controller (#5)

--- internal/infra/http/controllers/measurement_controller.go
+++ internal/infra/http/controllers/measurement_controller.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/domain"
+	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/http/requests"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/BohdanBoriak/boilerplate-go-back/internal/app"
 	"github.com/BohdanBoriak/boilerplate-go-back/internal/infra/database"
@@ -12,7 +16,7 @@ type MeasurementController struct {
 	measurementService app.MeasurementService
 }
 
-func NewMeasurementController(ds app.MeasurementService) MeasurementController {
+func NewMeasurementController(ms app.MeasurementService) MeasurementController {
 	return MeasurementController{
 		measurementService: ms,
 	}
@@ -26,21 +30,34 @@ func (mc MeasurementController) FindList() http.HandlerFunc {
 			return
 		}
 
-		f := database.MeasurementSearchParams{
-			From:   domain.LangCode(r.URL.Query().Get("from")),
-			To:     domain.LangCode(r.URL.Query().Get("to")),
-			Search: r.URL.Query().Get("search"),
-			Status: domain.TranslationRequestStatus(r.URL.Query().Get("status")),
+		var date time.Time
+		dateString := r.URL.Query().Get("date")
+		if dateString != "" {
+			date, err = time.Parse(time.DateTime, dateString)
+			if err != nil {
+				log.Printf("MeasurementController: %s", err)
+				BadRequest(w, err)
+				return
+			}
 		}
 
-		trs, err := mc.measurementService.MeasurementsList()
+		f := database.MeasurementSearchParams{
+			DeviceId:   r.Context().Value(DeviceKey).(domain.Device).Id,
+			Date:       &date,
+			Pagination: p,
+		}
+
+		trs, err := mc.measurementService.MeasurementsList(f)
 		if err != nil {
 			log.Printf("MeasurementController: %s", err)
 			InternalServerError(w, err)
 			return
 		}
 
-		var psDto resources.TranslationRequestsDto
-		Success(w, psDto.DomainToDtoPagination(trs))
+		fmt.Println(trs)
+
+		//todo: finish with resources
+		//var psDto resources.TranslationRequestsDto
+		//Success(w, psDto.DomainToDtoPagination(trs))
 	}
 }
